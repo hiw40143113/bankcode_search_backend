a08e410ccaec4bb289f66bd8f0ceaac01d8cceba
Wheat Wu <hiw40143113@gmail.com>
2024-06-29 14:34:26 +0800

    [added]add allowed_hosts

--- bankcode/settings.py
+++ bankcode/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://bankcode-search-7a93fdebedf9.herokuapp.com/"]
 
 
 # Application definition
